d7fd6429b1de53506446f103a6f24708b3054c77
Alexander William Verhulst <ftrr2017@gmail.com>
2019-11-21 11:27:21 +0200

    Adjusted the comments so that we can see all of them because they were too long at first

--- src/ds/queue/Queue.java
+++ src/ds/queue/Queue.java
@@ -18,19 +18,22 @@ public class Queue {
     }
 
     //create method #1 This method helps us put data into the end of the queue
-    public void insert(long j) { //we have a parameter that accepts data types of long because our array also accepts long
+    public void insert(long j) { //we have a parameter that accepts data types of long because our array also accepts
+        // long
 
         //This if statement that just recycles the 5 numbers in the queue is referred as a "CIRCULAR QUEUE".
         //The circular queue is just one type of implementation. Can also have it notify the user that they reached
         //the max size of the queue.
         if (rear == maxSize - 1) { //rear points to the index position of the length of array -1, if rear equals that
             rear = -1; //we just bring it back to beginning
-        } //when we insert a new element in the main, the new number will override and replace whatever is in the first position. since
+        } //when we insert a new element in the main, the new number will override and replace whatever is in the first
+        // position. since
         //we set slot size to 5, it just replaces the first slot. We will also make the second to last element the
         //new rear with rear = -1
 
 
-        rear++;//**Whenever we insert items into a queue, they go to the end of the array**. When we increment it goes from -1 to 0
+        rear++;//**Whenever we insert items into a queue, they go to the end of the array**. When we increment it goes
+        // from -1 to 0
         queArray[rear] = j; //we put the data at the 0 index position. That's why it is important to initialize at -1
         nItems ++;
     }
@@ -38,8 +41,10 @@ public class Queue {
     //create method #3. When we remove a method from the line, we remove it from the front of queue
     public long remove() {
 
-       long temp = queArray[front]; //first get element in front of line. we extract whatever is in the index position of the "front" value
-        front ++; //since we pulled the index value from the original front, we increment to create the new front position
+       long temp = queArray[front]; //first get element in front of line. we extract whatever is in the index position
+        // of the "front" value
+        front ++; //since we pulled the index value from the original front, we increment to create the new
+        // front position
         if (front == maxSize) { //if front is equal to the maxSize, that means we removed everything from the queue
             front = 0; //we can set front back to the 0th index so that we can utilize the entire array again
          }
@@ -49,7 +54,8 @@ public class Queue {
 
     //create method #4. We create this method to just find out what element is in the front
     public long peekFront() {
-        return queArray[front]; //return the queue array, at the position front. Front is a pointer, we are not changing the value, we are just observing who is at front
+        return queArray[front]; //return the queue array, at the position front. Front is a pointer, we are not
+        // changing the value, we are just observing who is at front
     }
 
     //create method #5. Create boolean method
